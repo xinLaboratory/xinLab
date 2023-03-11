9dfcc6c312f78d0ce2b0670fc72d57ad38f23543
dethMastery <me@suphakit.net>
2023-03-11 23:37:02 +0700

    feat: updated startup animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import ScrollAnimation from 'react-animate-on-scroll';
+
 import './App.css'
 
 import { Head } from './components/Head'
+import { Repo } from './components/Repo'
 
 function App() {
   return (
     <div className="App">
-      <Head name='kizune' bg=''></Head>
+      <Head name='kizune' bg='https://picsum.photos/1920/1080' />
+
+      <ScrollAnimation animateIn="fadeIn">
+        <Repo />
+      </ScrollAnimation>
     </div>
   )
 }
--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 export class Head extends React.Component {
   render() {
-    return <div className="middle">
-      <h1>
-        Welcome to {this.props.name}
-      </h1>
-    </div>;
+    return <span>
+      <div className="middle" id="topPage" style={{ background: `url(${this.props.bg})` }}>
+        <div className="overlay">
+          <h1 className="startup">
+            Welcome to {this.props.name}
+          </h1>
+        </div>
+      </div>
+    </span>;
   }
 }
