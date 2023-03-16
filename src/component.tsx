7d52f66cee9183f3af2ba3585a2d4bad88908c2f
dethMastery <me@suphakit.net>
2023-03-16 11:37:35 +0700

    feat: added Contact Part Information

--- src/component.tsx
+++ src/component.tsx
@@ -131,7 +131,16 @@ export class Contact extends React.Component {
   render() {
     return (
       <div className="box" id="Contact">
-
+        <h1 style={{textAlign: 'center'}}>
+          <u>
+            Contact
+          </u>
+        </h1>
+        <div className="contactBox">
+          <p>
+            E-mail: <a href="mailto:kizune@000198.xyz">kizune@000198.xyz</a>
+          </p>
+        </div>
       </div>
     )
   }
