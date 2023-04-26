c6e7d04840bb15bb9efd0f9d78f0898c1918f1a1
dethMastery <me@suphakit.net>
2023-04-27 02:22:53 +0700

    feat: added repo listing

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,6 +1,22 @@
 import React from "react";
 
+interface repoInterface {
+  id: number;
+  name: string;
+  link: string;
+  desp: string;
+}
+
 export class Repo extends React.Component {
+  RepoData: repoInterface[] = [
+    {
+      id: 0,
+      name: "CMMT 2023",
+      link: "https://cmmt.caffe.quest",
+      desp: "This Project just get started by `Mr. Tatsuya` and many of comment by various member in `GrassRoot Community` for `Chiang Mai MaiMai Tournament`",
+    },
+  ];
+
   render() {
     return (
       <div className="box" id="Repo">
@@ -14,15 +30,18 @@ export class Repo extends React.Component {
         </h1>
 
         <div className="container">
-          <div className="cardBox">
-            <a href="https://github.com/KIZUNEParty/tyDon">
-              <h1>tyDon</h1>
-            </a>
-            <p>
-              Just a laziest cli package that making for convert time to
-              epochtime.
-            </p>
-          </div>
+          {this.RepoData.map((repoData: repoInterface) => {
+            return (
+              <div className="cardBox" key={repoData.id}>
+                <a href={repoData.link}>
+                  <h1>{repoData.name}</h1>
+                </a>
+                <p>
+                  {repoData.desp}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     );
