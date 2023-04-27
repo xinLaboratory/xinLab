7ade6682072b60cdb15f71f978723dc23490f7f1
dethMastery <me@suphakit.net>
2023-04-27 10:40:04 +0700

    feat: a little animation in a worked box :)

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -3,6 +3,7 @@ import React from "react";
 interface repoInterface {
   id: number;
   name: string;
+  img: string;
   link: string;
   desp: string;
 }
@@ -12,6 +13,7 @@ export class Repo extends React.Component {
     {
       id: 0,
       name: "CMMT 2023",
+      img: "https://cdn.jsdelivr.net/gh/xinLaboratory/assets@master/Homepage/Works/cmmt2023.webp",
       link: "https://cmmt.caffe.quest",
       desp: "This Project just get started by `Mr. Tatsuya` and many of comment by various member in `GrassRoot Community` for `Chiang Mai MaiMai Tournament`",
     },
@@ -26,19 +28,23 @@ export class Repo extends React.Component {
             paddingBottom: "1rem",
           }}
         >
-          <u>Featured Repository</u>
+          <u>Proudly presented works</u>
         </h1>
 
         <div className="container">
           {this.RepoData.map((repoData: repoInterface) => {
             return (
-              <div className="cardBox" key={repoData.id}>
-                <a href={repoData.link}>
-                  <h1>{repoData.name}</h1>
-                </a>
-                <p>
-                  {repoData.desp}
-                </p>
+              <div
+                className="cardBox workCard"
+                key={repoData.id}
+                style={{ background: `url(${repoData.img})` }}
+              >
+                <div className="bib">
+                  <a href={repoData.link}>
+                    <h1>{repoData.name}</h1>
+                  </a>
+                  <p>{repoData.desp}</p>
+                </div>
               </div>
             );
           })}
